1698550ad651def7327137f731f38bdafde8e9dd
erfan <ahmederfan822@gmail.com>
2020-12-22 10:08:37 +0200

    functionality for adding the post from the admin site

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()   # reason why we created the publish function this is because some time we write the post but we don't post it when we will finally post then It will call this function.
+
+    # create a string representation
+    def __str__(self):
+        return self.title

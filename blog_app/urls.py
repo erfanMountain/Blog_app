426fd2d009c6cc1d4213beaf3a62434e306f21c9
erfan <ahmederfan822@gmail.com>
2021-02-09 22:41:28 +0200

    styling,adding the bootstrap,creating the base file,adding the url and views for post detail

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # here name attribute value is post_list which means it will take you to post_list page.
+    #127.0.0.1#8082/post/1 SO that pk means it will go to that specific post
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]

8e55b5d88a03dc5f7d880fb3d6c75e994e3cceee
erfan <ahmederfan822@gmail.com>
2021-02-14 22:05:17 +0200

    making the new feature creating the blog post

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'), # here name attribute value is post_list which means it will take you to post_list page.
     #127.0.0.1#8082/post/1 SO that pk means it will go to that specific post
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # when it is 127.0.0.1:8000/post/new --> local
+    # when it is mydjangosite.com/post/new --> online
+    path('post/new/', views.post_new, name='post_new'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from .forms import PostForm
 from .models import Post
 
 
@@ -7,10 +9,32 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     stuff_for_frontend = {'posts': posts}
-    return render(request, 'blog/post_list.html', stuff_for_frontend) #stuff_for_frontend here is the context dictionary
+    return render(request, 'blog/post_list.html',
+                  stuff_for_frontend)  # stuff_for_frontend here is the context dictionary
+
 
 def post_detail(request, pk):
     # we put the primary key in here this is because we can go for sepecific posts with their comments
-    post = get_object_or_404(Post, pk=pk) # it will get either specific posts or throw the 404
+    post = get_object_or_404(Post, pk=pk)  # it will get either specific posts or throw the 404
     stuff_for_frontend = {'post': post}
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            print(request.user)
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    # print(request) -> It will give the result the which request I used get or post
+    # print(request.__dict__) -> It will show many keys of the dictionary it will show lot of attributes
+    # print(request.method) -> it will show which request I am using
+    # print(request.POST) -> it will give what we posted as dictionary
+    else: # if someone do for  get request then do this below part
+        form = PostForm()
+    stuff_for_frontend = {'form': form}
+    return render(request, 'blog/blog_post_edit.html', stuff_for_frontend)
